f2cbbca62c6d1cf26dddb0310e4e6a8c833fff03
Ryan Chenkie <ryanchenkie@gmail.com>
2016-12-02 12:29:00 -0500

    base64 change prep. use non-decoded secret by default.

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -43,11 +43,13 @@ Knock.setup do |config|
   ## Default:
   # config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
 
-  ## If using Auth0, uncomment the line below
-  # config.token_secret_signature_key = -> { JWT.base64url_decode Rails.application.secrets.auth0_client_secret }
-  config.token_secret_signature_key = lambda {
-    secret = Rails.application.secrets.auth0_client_secret
-    secret += '=' * (4 - secret.length.modulo(4))
-    Base64.decode64(secret.tr('-_', '+/'))
-  }
+  ## Get client secret
+  config.token_secret_signature_key = -> { Rails.application.secrets.auth0_client_secret }
+
+  ## Uncomment below to Base64 decode secret if necessary
+  # config.token_secret_signature_key = lambda {
+  #   secret = Rails.application.secrets.auth0_client_secret
+  #   secret += '=' * (4 - secret.length.modulo(4))
+  #   Base64.decode64(secret.tr('-_', '+/'))
+  # }
 end
