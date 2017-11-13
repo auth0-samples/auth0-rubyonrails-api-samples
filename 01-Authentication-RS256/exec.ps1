49fdc9763b32bb695d4e7fdd9551186140a1a05d
Alexis Luque <aluque@indexlabs.com.uy>
2017-11-13 10:32:57 -0300

    Change port to 3010

--- 01-Authentication-RS256/exec.ps1
+++ 01-Authentication-RS256/exec.ps1
@@ -1,2 +1,2 @@
 docker build -t auth0-rubyonrails-api-rs256 .
-docker run --env-file .env -p 3000:3000 -it auth0-rubyonrails-api-rs256
+docker run --env-file .env -p 3010:3010 -it auth0-rubyonrails-api-rs256
