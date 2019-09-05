8ffea22c45a0ea8a925dba48b1013df7970c801f
Josh Cunningham <josh@joshcanhelp.com>
2019-09-05 10:23:34 -0700

    Update gems and remove database support

--- 01-Authentication-RS256/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class User < ApplicationRecord
-  def self.from_token_payload(payload)
-    payload['sub']
-    end
-end
--- 01-Authentication-RS256/config/environments/development.rb
+++ 01-Authentication-RS256/config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
+  # config.active_record.migration_error = :page_load
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
