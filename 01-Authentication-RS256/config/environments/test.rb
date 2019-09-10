b4c380ee3c26aebfeec9d23534b768a7b9466320
Josh Cunningham <josh@joshcanhelp.com>
2019-09-10 07:56:48 -0700

    Remove commented lines

--- 01-Authentication-RS256/config/environments/test.rb
+++ 01-Authentication-RS256/config/environments/test.rb
@@ -34,10 +34,4 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-
-  # Print deprecation notices to the stderr.
-  # config.active_support.deprecation = :stderr
-
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
 end
