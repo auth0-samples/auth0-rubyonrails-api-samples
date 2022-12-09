0a525defc4db425f2437250720ca1d2ebb127c95
Carla Urrea Stabile <carla.urrea@okta.com>
2022-12-09 11:52:18 +0100

    reduce frameworks to the minimum

--- 01-Authentication-RS256/config/application.rb
+++ 01-Authentication-RS256/config/application.rb
@@ -4,12 +4,12 @@ require_relative 'boot'
 require 'rails'
 
 # Pick the frameworks you want:
-require 'active_job/railtie'
+# require 'active_job/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
-require 'action_view/railtie'
-require 'action_cable/engine'
-require 'rails/test_unit/railtie'
+# require 'action_view/railtie'
+# require 'action_cable/engine'
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
